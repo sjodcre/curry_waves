ca8c266370b2cbf8eba62e1f928f226675de79e1
Your Name <you@example.com>
2024-12-09 16:37:42 +0800

    fixes for deployment

--- src/components/organisms/ProfileManage/types.ts
+++
@@ -1,7 +0,0 @@
-// import { ProfileHeaderType } from "@/context/ProfileContext";
-
-// export interface IProps {
-// 	profile: ProfileHeaderType | null;
-// 	handleClose: (handleUpdate: boolean) => void;
-// 	handleUpdate: () => void;
-// }
